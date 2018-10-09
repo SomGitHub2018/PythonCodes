54129f55111ff7dcd1de560a92aabff1377f22ae
someshGit <someshsahu@gmail.com>
2018-10-09 19:01:48 +0530

    Signed-off-by: someshGit <someshsahu@gmail.com> changes made to take inputs from user

--- Pythagorean triplet.py
+++ Pythagorean triplet.py
@@ -1,6 +1,11 @@
 # Check the enter 3 numbers are Pythagorean triplets are not.
-#nums = [1,2,3]
-nums = [4,5,3]
+#nums = [3,4,5] working
+# to take input from the user
+a= int(input("Enter a number a: "))
+b= int(input("Enter a number b: "))
+c= int(input("Enter a number c: "))
+
+nums = [a,b,c]
 h = max(nums)
 nums.remove(h)
 squares=[i ** 2 for i in nums]
